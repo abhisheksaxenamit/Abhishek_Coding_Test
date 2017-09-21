68b7b4c80cbfe09f3b2c9ebca77720c4ddcfa71c
abhisheksaxenamit <abhishek.saxena.mit@gmail.com>
2017-09-22 00:18:13 +0100

    Writing the o/p in json file
    Able to store information in Json file

--- Url_Request.py
+++ Url_Request.py
@@ -5,6 +5,9 @@ import json
 HeaderFile = open('Header.txt','w')
 r = requests.get("http://google.com")
 HeaderFile.write(str(r.headers))
+data=dict(r.headers)
+with open('Header.json', 'w') as outfile:
+    json.dump(data, outfile)
 ##r = requests.get("bad://address")
 #print("Res: ",r.content)
 print("Code: ",r.status_code)
