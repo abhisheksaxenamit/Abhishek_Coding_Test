9b0abbb92a1b912ea7ca65b8ad26adf38722a354
abhisheksaxenamit <abhishek.saxena.mit@gmail.com>
2017-09-22 01:55:41 +0100

    To store o/p for different urls
    Objective is to create multiple documents for all correct urls.
    
    Here bad urls are not considered.

--- Url_Request.py
+++ Url_Request.py
@@ -1,15 +1,40 @@
 import requests
 import json
 
+## Storing json for only valid urls.
 ## Trying to look at the information in Header and looking at Status Code
-HeaderFile = open('Header.txt','w')
-r = requests.get("http://google.com")
-HeaderFile.write(str(r.headers))
-data=dict(r.headers)
-with open('Header.json', 'w') as outfile:
-    json.dump(data, outfile)
-##r = requests.get("bad://address")
-#print("Res: ",r.content)
-print("Code: ",r.status_code)
+##HeaderFile = open('Header.txt','w')
+##Output={}
+count=0
+with open('Input.txt','r') as inp:
+    for url in inp:
+##        url = inp.readlines()
+        url=url.rstrip()
+        print(url)
+        try:
+            r = requests.get(url)
+            print("Code: ",r.status_code)
+            if(r.status_code == 200):
+                print("Code is 200 ")
+            ##    HeaderFile.write(str(r.headers))
+                data={'Url' : url, 'Status_code' : r.status_code}
+                data.update(dict(r.headers))
+            else:
+                print("Code is not 200 ")
+                data={'Url' : url, 'Status_code' : r.status_code}
+    ##        with open('Header.json') as f:
+    ##            Output = json.load(f)
+##            Output.update(data)
+        except:
+            print("ERROR!!")
 
-HeaderFile.close()
+        f_name= 'Url_'+str(count)+'.json'
+        count=count+1
+        print(f_name)
+        with open(f_name, 'w') as outfile:
+            json.dump(data, outfile)
+        ##r = requests.get("bad://address")
+        #print("Res: ",r.content)
+        
+
+    ##HeaderFile.close()
